6197aeac2a011f1e5c67d2ffc9c33fa8597c2870
phyllis <phyllis_roading@163.com>
2014-01-22 16:44:30 +0800

    LoginActivity is ok 等服务器接口

--- src/net/basilwang/trade/LoginActivity.java
+++ src/net/basilwang/trade/LoginActivity.java
@@ -2,34 +2,36 @@ package net.basilwang.trade;
 
 import net.basilwang.utils.NetworkUtils;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class LoginActivity extends Activity implements OnClickListener{
-	
+public class LoginActivity extends Activity implements OnClickListener {
+
 	private ProgressBar mProgress;
-	private EditText loginName,loginPsw;
-	private Button loginBtn,cancelBtn;
+	private EditText loginName, loginPsw;
+	private Button loginBtn, cancelBtn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		initView();
-		
+
 	}
 
 	private void initView() {
-		mProgress = (ProgressBar)findViewById(R.id.progress_bar);
-		loginName = (EditText)findViewById(R.id.login_name);
-		loginPsw = (EditText)findViewById(R.id.login_psw);
-		loginBtn = (Button)findViewById(R.id.login_btn);
+		mProgress = (ProgressBar) findViewById(R.id.progress_bar);
+		loginName = (EditText) findViewById(R.id.login_name);
+		loginPsw = (EditText) findViewById(R.id.login_psw);
+		loginBtn = (Button) findViewById(R.id.login_btn);
 		loginBtn.setOnClickListener(this);
-		cancelBtn = (Button)findViewById(R.id.cancel_btn);
+		cancelBtn = (Button) findViewById(R.id.cancel_btn);
 		cancelBtn.setOnClickListener(this);
 	}
 
@@ -43,20 +45,29 @@ public class LoginActivity extends Activity implements OnClickListener{
 		case R.id.cancel_btn:
 			this.finish();
 			break;
-			
+
 		default:
 			break;
 		}
 	}
-	
+
 	private void checkLogin() {
-		
-		
+		String name = loginName.getText().toString().trim();
+		String psw = loginPsw.getText().toString().trim();
+		if (name.equals("") || psw.equals("")) {
+			Toast.makeText(this, "请将登录信息填写完整", Toast.LENGTH_SHORT).show();
+		} else if (isNetAvailable()) {
+			Toast.makeText(this, "succedd", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(this, SalesAssisteantActivity.class);
+			startActivity(intent);
+		} else {
+			Toast.makeText(this, "网络未连接，请检查！", Toast.LENGTH_SHORT).show();
+		}
+
 	}
 
-	private Boolean isNetAvailable(){
-		return NetworkUtils.isConnect(this);
+	private Boolean isNetAvailable() {
+		return NetworkUtils.isConnect(this) ? true : false;
 	}
-	
 
 }
